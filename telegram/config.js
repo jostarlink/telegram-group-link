c5d9a3fffbd6147dae8937621dae2603d7f77cfd
MaStEr FeRi <feri.sys13@Gmail.com>
2016-02-14 21:19:59 +0330

    Update config.js

--- telegram/config.js
+++ telegram/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    botNickname: 'group_link_bot',
-    botToken: '93293084:AAFXvjSCkQalRi676fb2lILq0T3DYr-QnVs'
+    botNickname: 'Message_Sender',
+    botToken: '185003641:AAH0W8Vm_5ghV6Y5s8yvRxwCUdX5TxNXB2w'
 };
